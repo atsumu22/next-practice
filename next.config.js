8f7bc2607655cba7e6e1a90193642b41140f10c5
atsumu22 <w.atsumu27@gmail.com>
2024-11-06 20:45:26 +0900

    added sitemap

--- next.config.js
+++ next.config.js
@@ -23,6 +23,28 @@ const nextConfig = {
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        // セキュリティヘッダーを適用するパス
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
